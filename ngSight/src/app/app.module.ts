cc8ee818b3666b20ac8de1955a3e34e7e18c9d08
Miroslav Stojakovic <miroslav.miki.stojakovic@gmail.com>
2019-05-13 22:44:33 +0200

    Added bar-chart, line-chart, pie-chart components

--- ngSight/src/app/app.module.ts
+++ ngSight/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { SectionSalesComponent } from './section/section-sales/section-sales.component';
 import { SectionOrdersComponent } from './section/section-orders/section-orders.component';
 import { SectionHealthComponent } from './section/section-health/section-health.component';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { LineChartComponent } from './line-chart/line-chart.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +20,10 @@ import { SectionHealthComponent } from './section/section-health/section-health.
     SidebarComponent,
     SectionSalesComponent,
     SectionOrdersComponent,
-    SectionHealthComponent
+    SectionHealthComponent,
+    BarChartComponent,
+    LineChartComponent,
+    PieChartComponent
   ],
   imports: [
     BrowserModule,
