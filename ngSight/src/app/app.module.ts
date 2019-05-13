14bf53f869081ef2e3600275292f8aff8e7ef195
Miroslav Stojakovic <miroslav.miki.stojakovic@gmail.com>
2019-05-13 23:04:52 +0200

    Added styling for card and card-deck; Added card-deck to sales section

--- ngSight/src/app/app.module.ts
+++ ngSight/src/app/app.module.ts
@@ -9,9 +9,9 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { SectionSalesComponent } from './section/section-sales/section-sales.component';
 import { SectionOrdersComponent } from './section/section-orders/section-orders.component';
 import { SectionHealthComponent } from './section/section-health/section-health.component';
-import { BarChartComponent } from './bar-chart/bar-chart.component';
-import { LineChartComponent } from './line-chart/line-chart.component';
-import { PieChartComponent } from './pie-chart/pie-chart.component';
+import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
+import { LineChartComponent } from './charts/line-chart/line-chart.component';
+import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
 
 @NgModule({
   declarations: [
